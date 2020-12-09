568b832e14e054e327cdb53cc9cdae9854f71f04
Chad <chad187@gmail.com>
2020-12-09 12:20:35 -0800

    all the other files

--- sniper.py
+++ sniper.py
@@ -2,52 +2,66 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from getpass import getpass
 import time
 import winsound
+import sys
 
-frequency = 2500  # Set Frequency To 2500 Hertz
-duration = 3000  # Set Duration To 1000 ms == 1 second
-winsound.Beep(frequency, duration)
+scheduled_jobs = 0
+browser = None
 
-Username = '2096240502'
-Password = '7908'
+def getPassword() :
+	username = input('Username\n')
+	password = ''
+	if sys.stdin.isatty():
+		password = getpass('Password\n')
+	else:
+		password = input('Password...sorry it will be in the clear\n')
+	return username, password
 
-def login(browser) :
-	browser.get('https://login.frontlineeducation.com/login?signin=db9ca07520392d5d8f625c973b80174c&productId=ABSMGMT&clientId=ABSMGMT#/login')
-
+def login() :
+	browser.get('https://login.frontlineeducation.com/login?signin=db554d1e950518f569b0593e54dfbaa5&productId=ABSMGMT&clientId=ABSMGMT#/login')
+	WebDriverWait(browser, 3).until(EC.element_to_be_clickable((By.ID, 'Username')))
 	try :
 		assert 'Frontline - Sign In' in browser.title
 
 		elem = browser.find_element_by_name('Username')  # Find the search box
-		elem.send_keys(Username)
+		elem.send_keys(username)
 
 		elem = browser.find_element_by_name('Password')
-		elem.send_keys(Password + Keys.RETURN)
-		return waitForRedirect(browser)
-	except :
+		elem.send_keys(password + Keys.RETURN)
+		return waitForRedirect()
+	except Exception as e:
+		print('error line 37')
+		print(e)
+		sys.stdout.flush()
+		restart()
 		return False
 
-def availableJobs(browser) :
-	time.sleep(5)
-	waitForRedirect(browser)
-	print('in availableJobs after redirect')
-	try :
-		elem = browser.find_element_by_css_selector('span.av').text
-		if elem == '0' :
-			refresh(browser)
+def availableJobs() :
+	global scheduled_jobs
+	if waitForRedirect() :
+		try :
+			find_full_view()
+			scheduled_jobs = int(browser.find_element_by_id('ui-id-2').find_element_by_tag_name('span').text)
+			elem = browser.find_element_by_css_selector('span.av').text
+			if elem == '0' :
+				refresh()
+				return False
+			else :
+				checkJobs()
+				return True
+		except Exception as e:
+			print('error line 57')
+			print(e)
+			sys.stdout.flush()
+			restart()
 			return False
-		else :
-			checkJobs(browser)
-			return True
-	except :
-		browser.quit()
-		time.sleep(4)
-		startup()
-		return False
 
 def switch(job):
-	schools = ['Granada High School', 'Livermore High School', 'Del Valle Continuation High School']
-	days = ['Wed', 'Thu', 'Fri']
+	schools = ['West High School', 'Tracy High School', 'Kimball High School', 'Stein Continuation High School']
+	days = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri']
 	durations = ['Full Day', '04:05', '6:05']
 	titles = ['sdc']
 	detail = job.find_element_by_css_selector('tr.detail')
@@ -61,53 +75,90 @@ def switch(job):
 		return False
 	return False
 
-def checkJobs(browser) :
+def checkJobs() :
 	jobs = browser.find_element_by_id('availableJobs')
 	jobs = jobs.find_elements_by_css_selector('tbody.job')
 	
 	for job in jobs :
 		if switch(job) :
-			acceptJobs(browser, job)
+			acceptJobs(job)
 		else :
-			detail = job.find_element_by_css_selector('tr.detail')
-			school = detail.find_element_by_class_name('location').text
-			localTime = time.asctime( time.localtime(time.time()) )
-			print("Didn't like that job: " + school + " " + localTime)
+			acceptJobs(job) #auto accept everything
 
-	refresh(browser)
+			#detail = job.find_element_by_css_selector('tr.detail')
+			#school = detail.find_element_by_class_name('location').text
+			#localTime = time.asctime( time.localtime(time.time()) )
+			#print("Didn't like that job: " + school + " " + localTime)
+			#sys.stdout.flush()
+			#winsound.Beep(frequency, duration)
+
+	refresh()
 	return
-def acceptJobs(browser, job) :
+def acceptJobs(job) :
 	summary = job.find_element_by_css_selector('tr.summary')
 	# summary.find_element_by_class_name('rejectButton').click() 
 	summary.find_element_by_class_name('acceptButton').click()
 	detail = job.find_element_by_css_selector('tr.detail')
 	school = detail.find_element_by_class_name('location').text
 	localTime = time.asctime( time.localtime(time.time()) )
-	print('############-I accepted a job at: ' + school + "-###################" + localTime)
-	winsound.Beep(frequency, duration)
+	time.sleep(4)
+	new_job_count = int(browser.find_element_by_id('ui-id-2').find_element_by_tag_name('span').text)
+	if new_job_count > scheduled_jobs :
+		winsound.PlaySound('./austin_yeah.wav', winsound.SND_FILENAME)
+		print('############-I accepted a job at: ' + school + "-###################" + localTime)
+		scheduled_jobs = new_job_count
+	else :
+		winsound.PlaySound('./fail.wav', winsound.SND_FILENAME)
+		print('############-I tried to get the job but I failed: ' + school + "-###################" + localTime)
+
+	sys.stdout.flush()
 	return
 
-def waitForRedirect(browser) :
+def waitForRedirect() :
 	try :
 		return  WebDriverWait(browser, 20).until(EC.url_to_be('https://sub.aesoponline.com/Substitute/Home'), "URL never matched")
-	except :
+	except Exception as e:
+		print('error 121')
+		print(e)
+		sys.stdout.flush()
+		restart()
 		return False
 
-def refresh(browser) :
-	time.sleep(61)
+def find_full_view() :
+	try :
+		WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.ID, 'ui-id-2')))
+	except :
+		browser.find_element_by_link_text("Click Here to return to the 'Full View' of data.").click()
+		try :
+			WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, 'ui-id-2')))
+		except :
+			return Exception
+		
+
+def refresh() :
+	time.sleep(1)
 	browser.refresh()
-	waitForRedirect(browser)
-	print('in refresh, after the redirect')
-	availableJobs(browser)
+	if waitForRedirect() :
+		find_full_view()
+		print('in refresh, after the redirect')
+		sys.stdout.flush()
+		availableJobs()
 	return
 
-def startup() :
-	browser = webdriver.Firefox()
-	if login(browser) :
-		availableJobs(browser)
-	else :
-		browser.quit()
-		time.sleep(4)
-		startup()
+def restart() :
+	global browser
+	winsound.PlaySound('./doh.wav', winsound.SND_FILENAME)
+	browser.quit()
+	browser = None
+	startup()
 
+def startup() :
+	global browser
+	#browser = webdriver.Firefox() #if using firefox
+	browser = webdriver.Chrome('./chromedriver.exe')
+	winsound.PlaySound('./let_the_games_begin.wav', winsound.SND_FILENAME)
+	if login() :
+		availableJobs()
+
+username, password = getPassword()
 startup()
